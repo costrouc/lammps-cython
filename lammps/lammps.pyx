bb6d276dabcbca8c050b40b5a7819125c03ac7da
Christopher Ostrouchov <chris.ostrouchov@gmail.com>
2016-01-04 09:54:57 -0500

    Added command and version info

--- lammps/lammps.pyx
+++ lammps/lammps.pyx
@@ -1,7 +1,12 @@
 from libc.stdlib cimport malloc, free
+from libc.stdint cimport int64_t
 
 from mpi4py import MPI          # Automatically calls Init and Finalize
 cimport mpi4py.libmpi as mpi
+
+# bigint is of type int64_t
+# TODO do we need the constructors (excluding LAMMPS)?
+
 
 cdef extern from "input.h" namespace "LAMMPS_NS":
      cdef cppclass Input:
@@ -9,16 +14,34 @@ cdef extern from "input.h" namespace "LAMMPS_NS":
           char* one(const char*)
           void file(const char*)
 
+# cdef extern from "atom.h" namespace "LAMMPS_NS":
+#      cdef cppclass Atom:
+#           Atom(LAMMPS*) except +
+#           int64_t natoms
+
+cdef extern from "universe.h" namespace "LAMMPS_NS":
+     cdef cppclass Universe:
+          Universe(LAMMPS*, mpi.MPI_COMM) except +
+          const char* version
+          
+# cdef extern from "domain.h" namespace "LAMMPS_NS":
+#      cdef cppclass Domain:
+#           Domain(LAMMPS*) except +
+#           int box_exist
+#           int dimension
+#           int nonperiodic
+
 cdef extern from "lammps.h" namespace "LAMMPS_NS":
     cdef cppclass LAMMPS:
          LAMMPS(int, char**, mpi.MPI_Comm) except +
          Input* input
-         int num_package
+         Universe* universe
          
 
 cdef class Lammps:
     cdef LAMMPS *thisptr
-    def __cinit__(self, args):
+    def __cinit__(self):
+        args = [] # TODO reimplement properly
         cdef int argc = len(args)
         cdef char** argv = <char**>malloc(len(args) * sizeof(char*))
         cdef mpi.MPI_Comm comm = mpi.MPI_COMM_WORLD
@@ -28,13 +51,18 @@ cdef class Lammps:
 
         self.thisptr = new LAMMPS(argc, argv, comm)
 
-    def test(self):
-        self.thisptr.input.one('print "Test that LAMMPS works!"')
+    @property
+    def __version__(self):
+        return self.thisptr.universe.version
+
+    def command(self, cmd):
+        """Runs a LAMMPS command
+
+           command: str
+        """
+        self.thisptr.input.one(cmd)
 
     def __dealloc__(self):
         del self.thisptr
         # TODO should I free string?
-
-    def get_packages(self):
-        return self.thisptr.num_package
         
