a58dfb01e3eb831ebf7c863651bc71d9f4655a9d
Christopher Ostrouchov <chris.ostrouchov@gmail.com>
2016-01-09 14:31:28 -0500

    Setting config file.

--- setup.py
+++ setup.py
@@ -5,24 +5,35 @@ import numpy
 
 from distutils.core import setup
 from distutils.extension import Extension
+from configparser import ConfigParser
 
+lammps_config_file = 'lammps.cfg'
+lammps_config = ConfigParser()
+lammps_config.read(lammps_config_file)
 
-sourcefiles = ['lammps/lammps.pyx']
+include_dirs = [
+    mpi4py.get_include(),
+    numpy.get_include()
+]
+include_dirs.append(lammps_config.get('lammps', 'lammps_include_dir'))
+include_dirs.append(lammps_config.get('mpi', 'mpi_include_dir'))
 
-extension =  Extension('lammps',
-                       sources=sourcefiles,
-                       include_dirs=['/home/costrouc/software/lammps-7Dec15/src',
-                                     '/usr/lib/mpich/include',
-                                     mpi4py.get_include(),
-                                     numpy.get_include()],
-                       libraries=['lammps'],
-                       library_dirs=['/home/costrouc/.local/lib'],
-                       language='c++')
+libraries = [lammps_config.get('lammps', 'lammps_library')]
+library_dirs = [lammps_config.get('lammps', 'lammps_library_dir')]
+
+source_files = ['lammps/lammps.pyx']
+
+extensions =  Extension('lammps',
+                        sources=source_files,
+                        include_dirs=include_dirs,
+                        libraries=libraries,
+                        library_dirs=['/home/costrouc/.local/lib'],
+                        language='c++')
 
 setup(
     name='lammps',
     version='0.0.1',
     author='Christopher Ostrouchov',
-    description='A Cython Interface to LAMMPS',
-    ext_modules=cythonize(extension),
+    description='Pythonic Wrapper to LAMMPS',
+    ext_modules=cythonize(extensions),
 )
