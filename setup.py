69a295101e14a159c99f5f17aef3e3f57c3d6b7c
Christopher Ostrouchov <chris.ostrouchov@gmail.com>
2016-01-14 12:41:56 -0500

    REstructure setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,34 @@
 import mpi4py
 import numpy
+
 from Cython.Build import cythonize
 
 from distutils.core import setup
 from distutils.extension import Extension
+
 from configparser import ConfigParser
 
-lammps_config_file = 'lammps.cfg'
 lammps_config = ConfigParser()
-lammps_config.read(lammps_config_file)
+lammps_config.read('lammps.cfg')
 
 include_dirs = [
     mpi4py.get_include(),
-    numpy.get_include()
+    numpy.get_include(),
+    lammps_config.get('lammps', 'lammps_include_dir'),
+    lammps_config.get('mpi', 'mpi_include_dir')
 ]
-include_dirs.append(lammps_config.get('lammps', 'lammps_include_dir'))
-include_dirs.append(lammps_config.get('mpi', 'mpi_include_dir'))
 
 libraries = [lammps_config.get('lammps', 'lammps_library')]
 library_dirs = [lammps_config.get('lammps', 'lammps_library_dir')]
 
-extension = Extension('lammps.core',
-                      sources=['lammps/core.pyx'],
-                      include_dirs=include_dirs,
-                      libraries=libraries,
-                      library_dirs=['/home/costrouc/.local/lib'],
-                      language='c++')
+extension = Extension(
+    'lammps.core',
+    sources=['lammps/core.pyx'],
+    include_dirs=include_dirs,
+    libraries=libraries,
+    library_dirs=library_dirs,
+    language='c++'
+)
 
 setup(
     name='lammps',
