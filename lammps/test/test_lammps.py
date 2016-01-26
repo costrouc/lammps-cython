f421e0c98feb8e334e279bddcaef6f169d895ac2
Christopher Ostrouchov <chris.ostrouchov@gmail.com>
2016-01-26 15:39:30 -0500

    Lammps class fully tested

--- lammps/test/test_lammps.py
+++ lammps/test/test_lammps.py
@@ -1,69 +1,64 @@
-import pytest
-
-from lammps.core import Lammps
-
+import os
 import functools
 
-# TODO
-#   file
-#   run(int)
-#   time_step
-#   time
+import pytest
+import lammps
 
-# Redefine Lammps command-line args so no annoying 
-# logs or stdout
-Lammps_partial = functools.partial(Lammps, args=[
+
+# Redefine Lammps command-line args so no annoying logs or stdout
+Lammps = functools.partial(lammps.Lammps, args=[
     '-log', 'none',
     '-screen', 'none'
 ])
 
+
 # Lammps command line arguments
 def test_lammps_init_default_units():
-    lmp = Lammps_partial()
+    lmp = Lammps()
     assert lmp.units == 'lj'
 
 
-def test_lammps_init_set_units():
-    lmp = Lammps_partial(units='metal')
+def test_lammps_init_set_get_units():
+    lmp = Lammps(units='metal')
     assert lmp.units == 'metal'
 
 
 def test_lammps_init_invalid_units():
     with pytest.raises(ValueError):
-        Lammps_partial(units='invalid_units')
+        Lammps(units='invalid_units')
 
 
 # MPI_Comm comm (don't want to do mpi tests)
 def test_lammps_init_invalid_comm():
     with pytest.raises(TypeError):
-        Lammps_partial(comm='invalid_comm')
+        Lammps(comm='invalid_comm')
 
 
 # Style
 def test_lammps_init_default_style():
-    lmp = Lammps_partial()
+    lmp = Lammps()
     assert lmp.system.style == 'atomic'
 
 
-def test_lammps_init_set_style():
-    lmp = Lammps_partial(style='full')
+def test_lammps_init_set_get_style():
+    lmp = Lammps(style='full')
     assert lmp.system.style == 'full'
 
 
 def test_lammps_init_invalid_style():
     with pytest.raises(ValueError):
-        Lammps_partial(style='invalid_style')
+        Lammps(style='invalid_style')
 
 
 # version
 def test_lammps_version():
-    lmp = Lammps_partial()
+    lmp = Lammps()
     assert isinstance(lmp.__version__, str)
 
 
 # command
 def test_lammps_command():
-    lmp = Lammps_partial()
+    lmp = Lammps()
     lmp.command('timestep 2.0')
     assert lmp.dt == 2.0
 
@@ -73,7 +68,7 @@ def test_lammps_file(tmpdir):
     tmpfile = tmpdir.join("test_file.in")
     tmpfile.write("timestep 1.0\n")
 
-    lmp = Lammps_partial()
+    lmp = Lammps()
     lmp.file(str(tmpfile))
     assert lmp.dt == 1.0
 
@@ -83,41 +78,67 @@ def test_lammps_file_twice(tmpdir):
     tmpfile1.write("timestep 1.0\n")
 
     tmpfile2 = tmpdir.join("test_file2.in")
-    tmpfile2.write("units full")
+    tmpfile2.write("timestep 2.0\n")
 
-    lmp = Lammps_partial()
+    lmp = Lammps()
 
     lmp.file(str(tmpfile1))
     assert lmp.dt == 1.0
-    assert lmp.units == 'lj'
 
     lmp.file(str(tmpfile2))
-    assert lmp.dt == 1.0
-    assert lmp.units == 'full'
+    assert lmp.dt == 2.0
 
-# def test_run():
-#     lmp = Lammps_partial()
-#     lmp.file('in.test')
-#     lmp.run(100)
-#     .assertEqual(lmp.time_step, 100)
-#     .assertEqual(lmp.time, 100 * 0.006)
+
+# Run
+def test_lammps_run():
+    # This tests has a dependency of the
+    # LAMMPS example melt
+    # dt tested
+    # time step tested
+    # time tested
+    # This is hardly a unit test... (a better way?)
+
+    lmp = Lammps()
+    lmp.file(os.path.join(lammps.__path__[0], 'data', 'melt.in'))
+
+    assert lmp.dt == 0.005
+    assert lmp.time_step == 100
+    assert lmp.time == lmp.time_step * lmp.dt
+
+
+# time_step
+def test_lammps_default_time_step():
+    lmp = Lammps()
+    assert lmp.time_step == 0
+
+
+def test_lammps_set_get_time_step():
+    lmp = Lammps()
+    lmp.time_step = 100
+    assert lmp.time_step == 100
 
 
 # dt
 def test_lammps_default_dt():
-    lmp = Lammps_partial()
+    lmp = Lammps()
     assert lmp.dt == 0.005
 
 
-def test_lammps_set_dt():
-    lmp = Lammps_partial()
+def test_lammps_set_get_dt():
+    lmp = Lammps()
     lmp.dt = 13.0
     assert lmp.dt == 13.0
 
 
+# time
+def test_lammps_default_time():
+    lmp = Lammps()
+    assert lmp.time == 0.0
+
+
 # reset
 def test_lammps_reset():
-    lmp = Lammps_partial()
+    lmp = Lammps()
     lmp.dt = 13.0
     lmp.reset()
     assert lmp.dt == 0.005
