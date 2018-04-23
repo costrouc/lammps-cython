e067971d8d3d625d54d8f1ead38af9077f840090
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-04-23 15:59:12 -0400

    Making bootstrapable lammps python install

--- setup.py
+++ setup.py
@@ -1,41 +1,51 @@
-from configparser import ConfigParser
-
-import mpi4py
-import numpy
-
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
-from Cython.Build import cythonize
 # To use a consistent encoding
 from codecs import open
 from os import path
 
+# https://stackoverflow.com/questions/2379898/make-distutils-look-for-numpy-header-files-in-the-correct-place
+try:
+    from Cython.setuptools import build_ext
+except:
+    # If we couldn't import Cython, use the normal setuptools
+    # and look for a pre-compiled .c file instead of a .pyx file
+    from setuptools.command.build_ext import build_ext
+    ext_modules = [Extension("lammps.core", sources=['lammps/core.cpp'], language='c++')]
+else:
+    # If we successfully imported Cython, look for a .pyx file
+    ext_modules = [Extension("lammps.core", sources=['lammps/core.pyx'], language='c++')]
 
-lammps_config = ConfigParser()
-lammps_config.read('lammps.cfg')
+# https://stackoverflow.com/questions/2379898/make-distutils-look-for-numpy-header-files-in-the-correct-place
+class CustomBuildExtCommand(build_ext):
+    """build_ext command for use when numpy and mpi4py headers are needed."""
+    def run(self):
+        import numpy
+        import mpi4py
 
-include_dirs = [
-    mpi4py.get_include(),
-    numpy.get_include(),
-    lammps_config.get('lammps', 'lammps_include_dir'),
-    lammps_config.get('mpi', 'mpi_include_dir')
-]
+        # Add mpi4py, numpy, and custom headers to include_dirs
+        self.include_dirs.append(numpy.get_include())
+        self.include_dirs.append(mpi4py.get_include())
 
-# TODO: Should maybe include mpi_cxx, mpi, python3.4m
-libraries = [lammps_config.get('lammps', 'lammps_library'),
-             lammps_config.get('mpi', 'mpi_library')]
-library_dirs = [lammps_config.get('lammps', 'lammps_library_dir')]
+        # not sure if necissary
+        from configparser import ConfigParser
+        lammps_config = ConfigParser()
+        lammps_config.read('lammps.cfg')
+        self.include_dirs.append(lammps_config.get('lammps', 'lammps_include_dir'))
+        self.include_dirs.append(lammps_config.get('mpi', 'mpi_include_dir'))
 
-extensions = [
-    Extension(
-        'lammps.core',
-        sources=['lammps/core.pyx'],
-        include_dirs=include_dirs,
-        libraries=libraries,
-        library_dirs=library_dirs,
-        language='c++'
-    )
-]
+        self.libraries = [
+            lammps_config.get('lammps', 'lammps_library'),
+            lammps_config.get('mpi', 'mpi_library'),
+        ]
+
+        self.library_dirs = [
+            lammps_config.get('lammps', 'lammps_library_dir')
+        ]
+
+        # Call original build_ext command
+        build_ext.run(self)
+
 
 here = path.abspath(path.dirname(__file__))
 
@@ -65,7 +75,8 @@ setup(
     url='https://gitlab.com/costrouc/lammps-cython',
     download_url='https://gitlab.com/costrouc/lammps-cython/-/archive/master/lammps-cython-master.zip',
     keywords=['lammps', 'molecular dynamics', 'cython', 'wrapper', 'mpi'],
-    ext_modules=cythonize(extensions),
+    cmdclass={'build_ext': CustomBuildExtCommand},
+    ext_modules=Extension("lammps.core", sources=['lammps/core.pyx'], language='c++'),
     packages=find_packages(exclude=['docs', 'tests']),
     package_data={
         'lammps': ['data/*.in']
@@ -75,7 +86,7 @@ setup(
             'pylammps=lammps.__main__:main'
         ]
     },
-    scripts=['scripts/pylammps'],
     setup_requires=['pytest-runner', 'setuptools>=38.6.0'],
     tests_require=['pytest', 'pytest-cov'],
+    install_requires=['mpi4py', 'numpy'],
 )
