d324780cd4a41f36919429a1d96ed8d40cff26e4
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-04-26 15:49:26 -0400

    Adding package deployment script

--- setup.py
+++ setup.py
@@ -66,9 +66,9 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 
 setup(
-    name='lammps',
+    name='lammps-cython',
     version='0.2.1',
-    description='Pythonic Wrapper to LAMMPS',
+    description='Pythonic Wrapper to LAMMPS using cython',
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='Christopher Ostrouchov',
