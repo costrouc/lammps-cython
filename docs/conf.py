eb5f201b48cd625f55b72796ba2b437476a8f176
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-04-27 11:39:07 -0400

    bump version 0.3.4 and change docs.

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2018, Chris Ostrouchov'
 author = 'Chris Ostrouchov'
 
 # The short X.Y version
-version = ''
+version = '0.3'
 # The full version, including alpha/beta/rc tags
-release = '0.2.1'
+release = '0.3.4'
 
 
 # -- General configuration ---------------------------------------------------
@@ -76,7 +76,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
