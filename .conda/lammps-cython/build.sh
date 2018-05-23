87a44a7a2167d6193def0f7a0970cb60801dca12
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2018-05-23 15:40:28 -0400

    Refered to mpi4py for proper installation

--- .conda/lammps-cython/build.sh
+++ .conda/lammps-cython/build.sh
@@ -1,1 +1,16 @@
-python setup.py install
+#!/bin/bash
+
+cat > lammps.cfg <<EOF
+[lammps]
+lammps_include_dir = $PREFIX/include/lammps
+lammps_library_dir = $PREFIX/lib
+lammps_library = lammps
+
+[mpi]
+mpi_include_dir = $PREFIX/include
+mpi_library_dir = $PREFIX/lib
+mpi_library = mpi, mpi_cxx
+
+EOF
+
+$PYTHON setup.py install
--- setup.py
+++ setup.py
@@ -37,18 +37,6 @@ class build_ext(_build_ext):
         def config_to_list(key1, key2):
             return [s.strip() for s in lammps_config.get(key1, key2).split(',')]
 
-        print('-================')
-        print(numpy.get_include())
-        print(mpi4py.get_include())
-        print(os.environ['PREFIX'])
-        print('$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
-
-        # Check if in conda environment
-        if 'PREFIX' in os.environ:
-            prefix_dir = os.environ['PREFIX']
-            self.include_dirs.append(os.path.join(prefix_dir, 'include'))
-            self.library_dirs.append(os.path.join(prefix_dir, 'lib'))
-
         # Add mpi4py, numpy, and custom headers to include_dirs
         self.include_dirs.extend([
             numpy.get_include(),
