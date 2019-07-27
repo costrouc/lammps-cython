41c793e0b2c76d35b0577f1d0c3eb2250458b24c
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2019-07-27 12:41:18 -0400

    Fixing travisci script

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='lammps-cython',
-    version='0.6.0',
+    version='0.6.1',
     description='Pythonic Wrapper to LAMMPS using cython',
     long_description=long_description,
     long_description_content_type="text/markdown",
