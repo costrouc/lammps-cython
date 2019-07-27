9caf96d1648a17115ccc492c9cac9efea627fe05
Chris Ostrouchov <chris.ostrouchov@gmail.com>
2019-07-27 10:24:31 -0400

    Adding html_baseurl for docs

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,8 @@ version = '0.3'
 # The full version, including alpha/beta/rc tags
 release = '0.3.4'
 
+html_baseurl = "https://costrouc.github.io/lammps-cython/"
+
 
 # -- General configuration ---------------------------------------------------
 
